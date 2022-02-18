98b3665400613f3539bec89005058337e7fb44cf
amula42 <65457511+amula42@users.noreply.github.com>
2022-02-19 01:20:36 +0530

    reactexpress

---
+++ routes/usersRoute.js
@@ -0,0 +1,6 @@
+const express = require("express");
+router = express.Router();
+usersRoute = require("../controllers/usersControllers");
+
+router.get("/", usersRoute.usersController);
+module.exports = router;
